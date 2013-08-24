59a10bbe595f4b129f560182d77dcafd23c9c06e
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-24 18:22:17 -0300

    'naturalET' function for natural Energy Tendency of a TileType.

--- src/Tile/TileType.hs
+++ src/Tile/TileType.hs
@@ -3,8 +3,14 @@ module Tile.TileType
     ) where
 
 import qualified Components as C
+import Electric.Tendency
 
 -- | Specifies the kind of a tile and
 -- stores all the data about it.
 data TileType = InfSource C.SourceData -- ^ Infinite source of energy.
-              | Wire C.WireData        -- ^ Transports energy in packs.
+              | Wire                   -- ^ Transports energy in packs.
+
+-- | Natural Energy Tendency of a TileType.
+naturalET :: TileType -> EnergyTendency
+naturalET (InfSource _) = ET 1
+naturalET Wire          = ET 0
