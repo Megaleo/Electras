dddaae77be30a2e599f244a8d0bc8772dfd7805b
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 16:48:18 -0300

    Coordinates operations.

--- src/Tile/Coordinates.hs
+++ src/Tile/Coordinates.hs
@@ -17,3 +17,27 @@ ceilingDCoord (a, b) = (ceiling a, ceiling b)
 -- | Does the @floor@ function on a DCoord
 floorDCoord :: DCoord -> Coord
 floorDCoord (a, b) = (floor a, floor b)
+
+(|+|) :: Coord -> Coord -> Coord
+(x1,y1) |+| (x2,y2) = (x1 + x2, y1 + y2)
+
+(|-|) :: Coord -> Coord -> Coord
+(x1,y1) |-| (x2,y2) = (x1 - x2, y1 - y2)
+
+(|*|) :: Coord -> Int -> Coord
+(x1,y1) |*| m = (x1 * m, y1 * m)
+
+(|/|) :: Coord -> Int -> Coord
+(x1,y1) |/| d = (x1 `div` d, y1 `div` d)
+
+(||+||) :: DCoord -> DCoord -> DCoord
+(x1,y1) ||+|| (x2,y2) = (x1 + x2, y1 + y2)
+
+(||-||) :: DCoord -> DCoord -> DCoord
+(x1,y1) ||-|| (x2,y2) = (x1 - x2, y1 - y2)
+
+(||*||) :: DCoord -> Double -> DCoord
+(x1,y1) ||*|| m = (x1 * m, y1 * m)
+
+(||/||) :: DCoord -> Double -> DCoord
+(x1,y1) ||/|| d = (x1 / d, y1 / d)
