b0ceaa9a9a8f82037cb8159ce077c3043f2bc49d
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 15:50:23 -0300

    'spinToDirection' auxiliar function, transforms spin to direction.

--- src/Electric/Spin.hs
+++ src/Electric/Spin.hs
@@ -1,8 +1,15 @@
 module Electric.Spin where
 
+import qualified Electric.Direction as D
+
 -- | The spin of a pack can determinate
 -- what direction it will go if it encounter
 -- a 2 path decision.
 data Spin = ToLeft
           | ToRight
           deriving Eq
+
+-- | Converts Spin to a direction.
+spinToDirection :: Spin -> D.Direction
+spinToDirection ToLeft  = D.DirLeft
+spinToDirection ToRight = D.DirRight
