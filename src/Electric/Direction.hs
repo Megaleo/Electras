58410b00d543fb0b024fd30dd56bb85fd1002e8a
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-24 22:45:19 -0300

    Renaming Directions, adding "Dir" to the beggining of each direction.

--- src/Electric/Direction.hs
+++ src/Electric/Direction.hs
@@ -1,17 +1,17 @@
 module Electric.Direction where
 
-import TileType.Coord
+import Tile.Coordinates
 
 -- | Directions
-data Direction = Up
-               | Down
-               | Left
-               | Right
+data Direction = DirUp
+               | DirDown
+               | DirLeft
+               | DirRight
 
 -- | Turns a direction into a unitary
 -- coordinate, based on SDL coordinates system.
 directionCoord :: Direction -> Coord
-directionCoord Up    = (0,-1)
-directionCoord Down  = (0, 1)
-directionCoord Right = (1, 0)
-directionCoord Left  = (-1,0)
+directionCoord DirUp    = (0,-1)
+directionCoord DirDown  = (0, 1)
+directionCoord DirRight = (1, 0)
+directionCoord DirLeft  = (-1,0)
