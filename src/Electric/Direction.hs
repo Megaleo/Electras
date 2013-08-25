709a8d0e2f093ce6b99dba0631d665aa84dba327
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 15:40:26 -0300

    Direction functions.

--- src/Electric/Direction.hs
+++ src/Electric/Direction.hs
@@ -16,3 +16,22 @@ directionCoord DirUp    = (0,-1)
 directionCoord DirDown  = (0, 1)
 directionCoord DirRight = (1, 0)
 directionCoord DirLeft  = (-1,0)
+
+oppositeDir :: Direction -> Direction
+oppositeDir DirUp    = DirDown
+oppositeDir DirRight = DirLeft
+oppositeDir DirLeft  = DirRight
+oppositeDir DirDown  = DirUp
+
+-- | Returns the direction relative to Up.
+normalizeToUp :: Direction -> Direction -> Direction
+normalizeToUp DirUp dir         = dir
+
+normalizeToUp DirDown dir       = normalizeToUp DirUp $ oppositeDir dir
+
+normalizeToUp DirLeft DirUp     = DirLeft
+normalizeToUp DirLeft DirDown   = DirRight
+normalizeToUp DirLeft DirLeft   = DirDown
+normalizeToUp DirLeft DirRight  = DirUp
+
+normalizeToUp DirRight dir      = normalizeToUp DirLeft $ oppositeDir dir
