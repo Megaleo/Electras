38edc67ec71b605074675d86891b9bc6a15555db
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 15:08:29 -0300

    Removing all 'Components' files and dependencies.

--- src/Components.hs
+++
@@ -1,7 +0,0 @@
-module Components
-    ( module Components.Source
-    , module Components.Wire
-    ) where
-
-import Components.Source
-import Components.Wire
--- src/Tile/TileType.hs
+++ src/Tile/TileType.hs
@@ -2,9 +2,9 @@ module Tile.TileType
     ( TileType (..)
     ) where
 
-import qualified Components as C
+import qualified Electric as E
 
 -- | Specifies the kind of a tile and
 -- stores all the data about it.
-data TileType = InfSource Pack         -- ^ Infinite source of energy.
+data TileType = InfSource E.Pack         -- ^ Infinite source of energy.
               | Wire                   -- ^ Transports energy in packs.
