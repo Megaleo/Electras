ec7dfca75012f361e34fa49dd902698a74e60667
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 15:04:26 -0300

    Removing SourceData and symplifying InfSource.

--- src/Components/Source.hs
+++ src/Components/Source.hs
@@ -1,11 +1,1 @@
-module Components.Source
-    ( SourceData (..)
-    ) where
-
-import Electric.Pack
-import Electric.Direction
-
--- | The data to define a source of
--- energy, that sends a pack to its
--- output on a direction.
-data SourceData = SourceData Pack Direction
+module Components.Source where
--- src/Tile/TileType.hs
+++ src/Tile/TileType.hs
@@ -6,5 +6,5 @@ import qualified Components as C
 
 -- | Specifies the kind of a tile and
 -- stores all the data about it.
-data TileType = InfSource C.SourceData -- ^ Infinite source of energy.
+data TileType = InfSource Pack         -- ^ Infinite source of energy.
               | Wire                   -- ^ Transports energy in packs.
