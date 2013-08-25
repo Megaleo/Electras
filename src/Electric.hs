c1563e5e4b2e86729cb07180d34779848abae6f4
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 14:20:44 -0300

    Removing Tendency module and all dependencies.

--- src/Electric.hs
+++ src/Electric.hs
@@ -5,11 +5,10 @@ module Electric
 import Electric.Flow
 import Electric.Energy
 import Electric.Pack
-import Electric.Tendency
 
 -- | Stores all the data to find the flow
 -- and the energy in a single tile. It may be
 -- used when it matters the whole tile, not the
 -- type of it, for example.
 -- It might be removed for testing purposes.
-data ElectricData = ElectricData (Maybe Pack) EnergyTendency
+data ElectricData = ElectricData (Maybe Pack)
--- src/Tile/TileType.hs
+++ src/Tile/TileType.hs
@@ -3,14 +3,8 @@ module Tile.TileType
     ) where
 
 import qualified Components as C
-import Electric.Tendency
 
 -- | Specifies the kind of a tile and
 -- stores all the data about it.
 data TileType = InfSource C.SourceData -- ^ Infinite source of energy.
               | Wire                   -- ^ Transports energy in packs.
-
--- | Natural Energy Tendency of a TileType.
-naturalET :: TileType -> EnergyTendency
-naturalET (InfSource _) = ET 1
-naturalET Wire          = ET 0
