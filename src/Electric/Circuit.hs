3fd38e7a131a0ffac91e15b481b92c1445793c41
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 14:37:33 -0300

    Creating and initializing 'Electric.Circuit' module.

---
+++ src/Electric/Circuit.hs
@@ -0,0 +1,15 @@
+module Electric.Circuit
+    ( Circuit (..)
+    ) where
+
+import qualified Tile as T
+
+-- | A circuit is formed by sources, that provides energy,
+-- by transporters, to transport energy and consumers,
+-- that consumes energy. All have their own coordinates.
+-- It is a form of tile organization.
+data Circuit = Circuit { sources :: [T.CTile]
+                       , transporters :: [T.CTile]
+                       , consumers :: [T.CTile]
+                       }
+
