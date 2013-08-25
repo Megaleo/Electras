3b8895072d1f74caa3bbe7d3d0a7bea0a64528f2
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 15:13:08 -0300

    Adding 'Electric.Direction' module to 'Elecric.hs'.

--- src/Electric.hs
+++ src/Electric.hs
@@ -3,9 +3,11 @@ module Electric
     , module Electric.Energy
     , module Electric.Pack
     , module Electric.Spin
+    , module Electric.Direction
     ) where
 
 import Electric.Flow
 import Electric.Energy
 import Electric.Pack
 import Electric.Spin
+import Electric.Direction
