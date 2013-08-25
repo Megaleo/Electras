5c6f5d02937e9add3db7fbe7d8a94e85098e9fc4
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 14:32:39 -0300

    'CTile' type synonym as (Coord, Tile).

--- src/Tile.hs
+++ src/Tile.hs
@@ -3,8 +3,11 @@ module Tile
     ) where
 
 import Tile.TileType
+import qualified Tile.Coordinates as C
 import qualified Electric as E
 
 -- | Representation of a single tile in the screen.
 -- It stores the the type and maybe a pack.
 data Tile = Tile TileType (Maybe E.Pack)
+
+type CTile = (C.Coord, Tile)
