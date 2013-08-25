3d273b0efccacab675b7a8fbb5c76e2b2d89da6b
Leonardo <leonardo.a.lessa@gmail.com>
2013-08-25 14:28:29 -0300

    Removing ElectricData datatype.

--- src/Electric.hs
+++ src/Electric.hs
@@ -1,15 +1,11 @@
 module Electric
-    ( ElectricData (..)
+    ( module Electric.Flow
+    , module Electric.Energy
+    , module Electric.Pack
+    , module Electric.Spin
     ) where
 
 import Electric.Flow
 import Electric.Energy
 import Electric.Pack
 import Electric.Spin
-
--- | Stores all the data to find the flow
--- and the energy in a single tile. It may be
--- used when it matters the whole tile, not the
--- type of it, for example.
--- It might be removed for testing purposes.
-data ElectricData = ElectricData (Maybe Pack)
--- src/Tile.hs
+++ src/Tile.hs
@@ -6,5 +6,5 @@ import Tile.TileType
 import qualified Electric as E
 
 -- | Representation of a single tile in the screen.
--- It stores the the type and the total electric data.
-data Tile = Tile TileType E.ElectricData
+-- It stores the the type and maybe a pack.
+data Tile = Tile TileType (Maybe E.Pack)
