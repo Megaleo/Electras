1c7272a39f9166f4fe0659a14749cbdda6ec8b9c
Leonardo <leonardo.a.lessa@gmail.com>
2013-11-02 15:49:38 -0200

    Changing circuit to a list of "CTile"s

--- src/Electric/Circuit.hs
+++ src/Electric/Circuit.hs
@@ -8,8 +8,4 @@ import qualified Tile as T
 -- by transporters, to transport energy and consumers,
 -- that consumes energy. All have their own coordinates.
 -- It is a form of tile organization.
-data Circuit = Circuit { sources :: [T.CTile]
-                       , transporters :: [T.CTile]
-                       , consumers :: [T.CTile]
-                       }
-
+type Circuit = [CTile]
